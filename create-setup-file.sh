1a10547f3b32b33f38b091e8c6a35d8047f0256c
bmeatayi <bmeatayi@gmail.com>
2022-01-19 11:50:06 +0100

    add bash script

---
+++ create-setup-file.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Check we are in master branch
+current_branch=$(git branch --show-current)
+#if [[ "$current_branch" != "master" ]]
+#then
+#  echo "Please checkout to master branch and try again!"
+#  exit
+#fi
+
+# Install Bluetooth headers on Ubuntu
+uname=$(uname);
+if [[ "$uname" == "Linux" ]]
+then
+  echo "Installing bluetooth headers!"
+  sudo apt-get update -yq
+  sudo apt-get install -yq libbluetooth-dev
+fi
+
+# Conda virtual env
+conda config --append channels conda-forge
+conda install -y gcc
+conda create -n gui_installer python=3.8.10 -y
+conda activate gui_installer
+which python
+python -m pip install --upgrade pip
+
+# Install qt and qt-ifw
+pip install aqtinstall
+aqt install-qt linux desktop 6.2.1 -m all
+aqt install-tool linux desktop tools_ifw
+aqt install-tool linux desktop tools_maintenance
+
+# Install Pyinstaller
+pip install pyinstaller==4.7
+
+# Install ExploreGUI
+pip install -e .
+
+# Create executable files
+pyinstaller --onedir --console ExploreGUI.spec
+
+# Copy files to data dir
+exploregui_path="installer/ExploreGuiInstaller/ExploreGUI/packages/com.Mentalab.ExploreGUI/"
+cp -r dist/ExploreGUI "$exploregui_path"data
+if [[ "$uname" == "Linux" ]]
+then
+  cp "$exploregui_path"extras/MentalabLogo.png "$exploregui_path"data/
+  cp "$exploregui_path"extras/exploregui.desktop "$exploregui_path"data/
+fi
+
+
+# Extensions
+if [[ "$uname" == "Linux" ]]
+then
+  extension=".run"
+else
+  extension=""
+fi
+./Tools/QtInstallerFramework/4.2/bin/binarycreator -c "$exploregui_path"../../config/config.xml -p "$exploregui_path"../ --verbose ExploreGUIInstaller"$extension"
