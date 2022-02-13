3037e2d46305a703dad7ba2a1654736a6edd0cac
Andrea Escartin <escartinandrea@gmail.com>
2022-02-13 21:23:03 +0100

    update dialogs ui

--- exploregui/modules/ui/dialog_recording_settings.py
+++ exploregui/modules/ui/dialog_recording_settings.py
@@ -1,89 +1,29 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'dialog_recording_settings_dark.ui'
+## Form generated from reading UI file 'dialog_recording_settings_light.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.1.0
+## Created by: Qt User Interface Compiler version 6.2.1
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import *
-from PySide6.QtGui import *
-from PySide6.QtWidgets import *
-
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
+    QHBoxLayout, QLabel, QLineEdit, QPushButton,
+    QRadioButton, QSizePolicy, QSpinBox, QVBoxLayout)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         if not Dialog.objectName():
             Dialog.setObjectName(u"Dialog")
         Dialog.resize(400, 300)
-        Dialog.setStyleSheet(u"QWidget{\n"
-"	font: 11pt \"DM Sans\";\n"
-"}\n"
-"\n"
-"QDialog{\n"
-"	background-color: rgb(28, 30, 42);\n"
-"}\n"
-"\n"
-"QLabel{\n"
-"	color: #FFF;\n"
-"}\n"
-"\n"
-"QLabe#label_4l{\n"
-"	font: italic 9pt \"DM Sans\";\n"
-"}\n"
-"\n"
-"QCheckBox{\n"
-"	color: #FFF\n"
-"}\n"
-"\n"
-"QLineEdit{\n"
-"	color: #FFF;\n"
-"	background-color: transparent;\n"
-"	/*background-color: rgb(50, 53, 74);\n"
-"	border: 1px solid rgb(84, 89, 124);*/\n"
-"	border: 1px solid #FFF;\n"
-"}\n"
-"\n"
-"QComboBox{\n"
-"	color: #FFF;\n"
-"	border: 1px solid #FFF;\n"
-"	background-color: transparent;\n"
-"}\n"
-"\n"
-"QRadioButton{\n"
-"	color: #FFF;\n"
-"	background-color: rgb(28, 30, 42);\n"
-"}\n"
-"\n"
-"QSpinBox{\n"
-"	color: #FFF;\n"
-"	border: 1px solid #FFF;\n"
-"	background-color: transparent;\n"
-"}\n"
-"\n"
-"QPushButton{\n"
-"	color: #FFF;\n"
-"	background-color: rgb(84, 89, 124);\n"
-"	border: 2px solid rgb(84, 89, 124);\n"
-"	padding: 5px;\n"
-"	border-radius: 5px;\n"
-"	width: 75px;\n"
-"	height: 15px;\n"
-"\n"
-"}\n"
-"\n"
-"QPushButton:hover{\n"
-"	background-color: rgb("
-                        "61, 64, 89);\n"
-"}\n"
-"\n"
-"QPushButton:pressed{\n"
-"	background-color: rgb(101, 106, 141);\n"
-"	border:  2px solid rgb(61, 64, 89);\n"
-"}\n"
-"")
         self.verticalLayout_2 = QVBoxLayout(Dialog)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.verticalLayout_2.setContentsMargins(-1, 25, -1, -1)
@@ -123,7 +63,6 @@ class Ui_Dialog(object):
 
         self.btn_browse = QPushButton(Dialog)
         self.btn_browse.setObjectName(u"btn_browse")
-        self.btn_browse.setCursor(QCursor(Qt.PointingHandCursor))
 
         self.horizontalLayout.addWidget(self.btn_browse)
 
@@ -148,18 +87,14 @@ class Ui_Dialog(object):
         self.label_4 = QLabel(Dialog)
         self.label_4.setObjectName(u"label_4")
         font = QFont()
-        font.setFamilies([u"DM Sans"])
-        font.setPointSize(11)
-        font.setBold(False)
-        font.setItalic(False)
+        font.setPointSize(10)
+        font.setItalic(True)
         self.label_4.setFont(font)
 
         self.verticalLayout.addWidget(self.label_4)
 
         self.buttonBox = QDialogButtonBox(Dialog)
         self.buttonBox.setObjectName(u"buttonBox")
-        self.buttonBox.setCursor(QCursor(Qt.PointingHandCursor))
-        self.buttonBox.setStyleSheet(u"")
         self.buttonBox.setOrientation(Qt.Horizontal)
         self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
 
@@ -182,7 +117,7 @@ class Ui_Dialog(object):
         self.rdbtn_csv.setText(QCoreApplication.translate("Dialog", u"csv", None))
         self.rdbtn_edf.setText(QCoreApplication.translate("Dialog", u"edf", None))
         self.lbl_step1.setText(QCoreApplication.translate("Dialog", u"2. Select the folder and name to store the file:", None))
-        self.btn_browse.setText(QCoreApplication.translate("Dialog", u"Browse", None))
+        self.btn_browse.setText(QCoreApplication.translate("Dialog", u"Broswe", None))
         self.label.setText(QCoreApplication.translate("Dialog", u"3. Select recording time (s):", None))
         self.label_4.setText(QCoreApplication.translate("Dialog", u"If recording time is 0, the default (3600 sec) will be used", None))
     # retranslateUi
