5b0af8e59a62f4b43e8a033d0e47731df5c6201a
Andrea Escartin <escartinandrea@gmail.com>
2022-03-07 09:39:46 +0100

    fix scaling issue

--- exploregui/main.py
+++ exploregui/main.py
@@ -1,11 +1,12 @@
 # This Python file uses the following encoding: utf-8
 
 import logging
+import os
 import sys
 
 import explorepy
 from PySide6.QtWidgets import QApplication
-
+from PySide6 import QtCore
 
 import exploregui  # isort:skip
 from exploregui import MainWindow  # isort:skip
@@ -14,8 +15,11 @@ logger = logging.getLogger("explorepy.exploregui.main")
 logger.debug("Starting ExploreGUI (version: %s) with Explorepy (version: %s)",
              exploregui.__version__, explorepy.__version__)
 
+os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "0"
+
 
 def main():
+    QApplication.setHighDpiScaleFactorRoundingPolicy(QtCore.Qt.HighDpiScaleFactorRoundingPolicy.Floor)
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
