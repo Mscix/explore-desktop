9114ed77381033cbc8abf80c63c4ae51d7a2bb3c
Andrea Escartin <escartinandrea@gmail.com>
2022-11-28 17:05:03 +0100

    add ui

--- exploredesktop/modules/ui/__init__.py
+++ exploredesktop/modules/ui/__init__.py
@@ -1,3 +1,6 @@
+from .dialog_convert_bin import Ui_Dialog as Ui_BinDialog
 from .dialog_plot_settings import Ui_Dialog as Ui_PlotDialog
 from .dialog_recording_settings import Ui_Dialog as Ui_RecordingDialog
 from .ui_main_window import Ui_MainWindow
+
+__all__ = ["Ui_MainWindow", "Ui_PlotDialog", "Ui_RecordingDialog", "Ui_BinDialog"]
