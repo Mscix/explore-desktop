e7f3f51ddd3773998573c765e550ee3d75e865ca
Andrea Escartin <escartinandrea@gmail.com>
2023-01-11 11:59:22 +0100

    add ui dialog

--- exploredesktop/modules/ui/__init__.py
+++ exploredesktop/modules/ui/__init__.py
@@ -2,6 +2,7 @@ from .dialog_convert_bin import Ui_Dialog as Ui_BinDialog
 from .dialog_plot_settings import Ui_Dialog as Ui_PlotDialog
 from .dialog_recording_settings import Ui_Dialog as Ui_RecordingDialog
 from .ui_main_window import Ui_MainWindow
+from .ui_dialog_repair_data import Ui_Dialog as Ui_RepairDialog
 
 
 __all__ = ["Ui_MainWindow", "Ui_PlotDialog", "Ui_RecordingDialog", "Ui_BinDialog"]
