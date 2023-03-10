11dd6ca853cd6f5aa34411980db61a084938d4be
Mscix <msci@gmx.net>
2023-03-10 11:53:56 +0100

    First tests for BT module, test_connect_turns_connecting is till in progress same as impedance

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,22 @@
+import pytest
+import PySide6
+from pytestqt.qtbot import QtBot
+from PySide6.QtTest import QTest
+import exploredesktop.main_window as mw
+
 
 
 class TestMainWindow:
-    def test_dummy_1(self):
-        assert 1 == 1
+    def test_close_window(self, qtbot):
+        window = mw.MainWindow()
+        qtbot.addWidget(window)
+        window.show()
+        assert window.isVisible()
+        QTest.qWaitForWindowExposed(window)
+        window.close()
+        assert not window.isVisible()
 
-    def test_dummy_2(self):
-        assert 2 == 2
+
+
+
+
