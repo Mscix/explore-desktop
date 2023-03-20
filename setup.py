0a3bde18c3caf10b4aecd35fa3c91ef2c5f90224
Andrea Escartin <escartinandrea@gmail.com>
2023-03-20 17:02:37 +0100

    update version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     test_suite='tests',
     extras_require=extras,
     url='https://github.com/Mentalab-hub/explore-desktop',
-    version='0.6.0',
+    version='0.7.0',
     zip_safe=False,
     entry_points={'console_scripts': ['exploredesktop = exploredesktop.main:main']},
 )
